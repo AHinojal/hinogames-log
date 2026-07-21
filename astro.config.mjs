c927ae66ebc2e7bed2e370085aacd460990ce5e9
Álvaro Hinojal <alvarohinojal@gmail.com>
2026-07-21 15:13:10 +0200

    Fix: Change config to load gifs in markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import remarkBaseImages from './plugins/remark-base-image.mjs';
+
+const base = "/hinogames-log/";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ahinojal.github.io",
-  base: "/hinogames-log/",
+  base,
+  markdown: {
+    remarkPlugins: [[remarkBaseImages, { base }]]
+  }
 });
